96142482509b7125ec60cf94da9b81ee5e087408
Amir <amir@fortinet>
2015-07-21 18:37:16 -0700

    -

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ Kaggle west-nile-virus completition
 import pandas as pd
 import numpy as np
 import pdb
-
+import datetime
 
 def load_weather():
     weather = pd.read_csv('weather.csv', na_values = ['M', '-'])  
@@ -70,7 +70,37 @@ def load_weather():
             
     return weather
 
-if __name__ == '__main__':
-    weather = load_weather()
+def dist(point1, point2):
+    return np.sum((point1 - point2)**2)
+    
+# Weather stations latitude and longitude
+station1 = [41.995, -87.933]
+station2 = [41.786, -87.752] 
+
+def find_closest_weather_station(lat, long):
+    point1 = np.array([lat, long])
+    return np.argmin([dist(point1, np.array(station1)), dist(point1, np.array(station2))]) + 1
+    
+def load_train():
+    train = pd.read_csv('train.csv')
+    weather_columns = weather.columns.tolist()
+    weather_columns.remove('Date')
+    train_columns = train.columns.tolist()
+    train_columns.remove('Date')
+    
+    # Add closest station for each trap
+    train['Station'] = [find_closest_weather_station(lat, long) for lat, long in zip(train.Latitude, train.Longitude)]
+    # Add weather date for the past and also the following 7 days
+    for day in (range(-7, 0) + range(1, 8)):
+        train['Date-new'] = [(datetime.date(y, m, d) - datetime.timedelta(day)).strftime("20%y-%m-%d") for y, m, d in [map(lambda x : int(x), date.split('-')) for date in train.Date]]
+        train = pd.concat([train, pd.merge(train, weather, left_on = ['Date-new', 'Station'], right_on = ['Date', 'Station'])[train_columns + weather_columns]], axis = 1)
+    return train1
+
         
+     
+
+if __name__ == '__main__':
+    pdb.set_trace()
+    weather = load_weather()
+    train = load_train()    
             
